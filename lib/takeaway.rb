93f60579de87f1d3d69886144766fe33e1c6a110
Maria Vanaale <mvanaale@gmail.com>
2019-02-17 21:46:50 +0000

    checks items and gets prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,18 +1,32 @@
 class Restaurant
-    attr_reader :menu
+    attr_reader :menu, :order, :total
 
     def initialize
-        @menu = { burger: 5,
-                  pizza: 6,
-                  lasagna: 4,
-                  ice_cream: 2
+        @menu = { 'burger': 5,
+                  'pizza': 6,
+                  'lasagna': 4,
+                  'ice_cream': 2
                 }
-                
-        @menu = menu
+        @order = []
+        @total = 0
     end 
 
     def menu_print
         @menu.each { |key, value| puts "#{key}: £#{value}" }
     end 
-     
+
+    def add_item(item_from_menu)
+        @order << item_from_menu
+        @total +=1
+    end 
+
+    def include?(item)
+        @menu.include?(item)
+    end
+
+    def  price(item)
+        @menu.fetch(item).to_f
+     end 
+    
+
 end 
